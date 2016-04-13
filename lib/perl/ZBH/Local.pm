8fa47d3dc201c629ae271f654019a82561cb6419
wurst <wurst@weissensee.zbh.uni-hamburg.de>
2016-04-13 17:37:50 +0200

    =update

--- lib/perl/ZBH/Local.pm
+++ lib/perl/ZBH/Local.pm
@@ -10,10 +10,27 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 package ZBH::Local;
-@EXPORT    = qw(is_background_process);
+@EXPORT = qw(is_background_process);
 use strict;
 use warnings;
 use Data::Dump qw( dump pp );
+
+sub is_file_exists {
+	my $file = shift;
+	return ( ( -d $file ) || ( -e $file ) );
+}
+
+sub is_folder_empty {
+	opendir( DIR, shift ) or die $!;
+	my @files = grep { !m/\A\.{1,2}\Z/ } readdir(DIR);
+	closedir(DIR);
+	@files ? 0 : 1;
+}
+
+sub is_folder_exists {
+	my $folder = shift;
+	return is_file_exists($folder);
+}
 
 sub is_background_process ($) {
 	my $starter = shift;
--- lib/perl/ZBH/SGE.pm
+++ lib/perl/ZBH/SGE.pm
@@ -10,7 +10,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 package ZBH::SGE;
-@EXPORT    = qw(is_background_process_sge is_background_process_started_sge is_background_process_done);
+@EXPORT    = qw(is_background_process is_background_process_sge is_background_process_started_sge is_background_process_done);
 use strict;
 use warnings;
 use Data::Dump qw( dump pp );
@@ -20,19 +20,19 @@ sub is_file_exists {
 	return ( ( -d $file ) || ( -e $file ) );
 }
 
-sub is_folder_empty {
-    my $dirname = shift;
-    opendir(my $dh, $dirname) or die "Not a directory";
-    return scalar(grep { $_ ne "." && $_ ne ".." } readdir($dh)) == 0;
-}
-
-sub is_background_process_sge ($) {
+sub is_background_process ($) {
 	my $starter = shift;
 	my $name = substr( $starter, rindex( $starter, "/" ) + 1 );
 
 	my $result = `qstat -r -ext`;
+	print($result);
 
 	return ( index( $result, $name ) > -1 );
+}
+
+sub is_background_process_sge ($) {
+	my $starter = shift;
+	return is_background_process($starter);
 }
 
 
@@ -53,7 +53,7 @@ sub is_background_process_done ($ $) {
 	return 0 if (not is_file_exists($logfile));	
 
 	my $result = `tail -n 2 $logfile`;
-	return ( index( $result, 'wurst-vector done' ) > -1 );
+	return ( index( $result, $keystring ) > -1 );
 }
 
 1;
